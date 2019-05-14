e58cfa7f8ffd2f06ff029122854fab7e39f22ab1
Leopiron <sourovsarkar84@gmail.com>
2019-05-15 00:07:02 +0530

    Bubble Sort

---
+++ Day 5/Q18/BubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>//Ascending order //Bubble sort
+int main()
+{
+	int i,n,ar[30],j,t;
+	printf("Enter the number of values you want to enter:\n");
+	scanf("%d",&n);
+	printf("Enter the numbers:\n");
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&ar[i]);
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1+i;j<=n;j++)
+		{
+			if(ar[i]>ar[j])
+			{
+	            t=ar[j];
+	            ar[j]=ar[i];
+	            ar[i]=t;
+			}
+		}
+	}
+	printf("your sorted array:\n");
+	for(int i=1;i<=n;i++)
+	{
+		printf("%d",ar[i]);
+	}
+	return 0;
+}
